75898023c1de0324663854718bb0078e1d821185
Arwa Sharif <arwas@google.com>
2024-08-09 10:34:33 -0500

    remove env file

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiKey :'1489b29bd01f9cf535c11238d7c22d76',
-    bearerToken: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDg5YjI5YmQwMWY5Y2Y1MzVjMTEyMzhkN2MyMmQ3NiIsIm5iZiI6MTcyMjAwODY1NC44MjY2NzQsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FyHLUYDRTyN-6rbCsr3x7EhzZWOX1susMDPKFr6opQM',
-    baseUrl: 'https://api.themoviedb.org/3'
-};
